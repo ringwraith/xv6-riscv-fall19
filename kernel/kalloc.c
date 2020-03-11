a66219d8a3bac6bb40fb98d4b62a198e56c63581
chyuwei <weichangyuwcy@gmail.com>
2020-03-11 10:59:48 +0800

    lab lock kalloc

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -21,12 +21,13 @@ struct run {
 struct {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+} kmem[NCPU];
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  for(int i = 0; i < NCPU; i++)
+    initlock(&kmem[i].lock, "kmem");
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -56,10 +57,13 @@ kfree(void *pa)
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  push_off();
+  int cpu = cpuid();
+  acquire(&kmem[cpu].lock);
+  r->next = kmem[cpu].freelist;
+  kmem[cpu].freelist = r;
+  release(&kmem[cpu].lock);
+  pop_off();
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -69,12 +73,34 @@ void *
 kalloc(void)
 {
   struct run *r;
+  struct run *tmp;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
+  push_off();
+  int cpu = cpuid();
+  acquire(&kmem[cpu].lock);
+  r = kmem[cpu].freelist;
   if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+    kmem[cpu].freelist = r->next;
+  release(&kmem[cpu].lock);
+  pop_off();
+  
+  if(!r) {
+    for(int i = 0; i < NCPU; i++)
+      acquire(&kmem[i].lock);
+
+    for(int i = 0; i < NCPU; i++){
+      tmp = kmem[i].freelist;
+      if(tmp){
+        // steal 1 page, maybe more
+        r = kmem[i].freelist;
+        if(r){
+          kmem[i].freelist = r->next;
+        }
+      }
+    }
+    for(int i = 0; i < NCPU; i++)
+      release(&kmem[i].lock);
+  }
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
